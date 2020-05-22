516f3961cac958a5dc039b97e9240e5ff1b447a0
3038922 <31612534@qq.com>
2020-05-22 16:35:44 +0800

    fist update
    Signed-off-by: 3038922 <31612534@qq.com>

---
+++ main.py
@@ -0,0 +1,49 @@
+# encoding='UTF-8'
+import sys
+import openpyxl  # excel操作模块
+import configparser  # 导入配置.ini模块
+from selenium import webdriver
+import time
+# 导入Select类
+from selenium.webdriver.support.ui import Select
+
+
+def main(argv=None):
+
+    config = configparser.ConfigParser()  # 类实例化
+    config.read('config.ini', encoding='UTF-8')
+    url = config.get('select', 'url')
+    userId = config.get('select', 'id')
+    userPasswd = config.get('select', 'passwd')
+    if url == "":
+        url = input("请输入你要登录的网址")
+    driver = webdriver.Chrome('./chromedriver.exe')
+    driver.implicitly_wait(10)
+    driver.get(url)
+    if userId == "":
+        userId = input("请输入您的身份证:")
+    if userPasswd == "":
+        userPasswd = input("请输入您的密码:")
+    print("您输入的的账号为:", userId, " 密码为:", userPasswd)
+    # login
+    ele = driver.find_element_by_css_selector('[placeholder="请输入身份证号"]')
+    ele.send_keys(userId)
+    ele = driver.find_element_by_css_selector('input[type="password"]')
+    ele.send_keys(userPasswd)
+    verificationCode = input("请输入验证码:")
+    ele = driver.find_element_by_css_selector('input[class = "login_yzm"]')
+    ele.send_keys(verificationCode)
+    ele = driver.find_element_by_css_selector('input[name = "login"]')
+    ele.click()
+
+    # test
+    ele = driver.find_element_by_css_selector(
+        '#JZGZDXSCJJSHJXX_addBtn [class="ui-button-text"]')
+    ele.click()
+    # find excel
+    input()
+    # input data
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ readme.md
@@ -0,0 +1,19 @@
+# 中国教师信息管理系统批量录入
+- 该项目旨在帮助个人信息较多的老师通过EXCEL 批量录入信息.
+
+## 使用说明
+
+
+## 开发环境安装
+- 如果要协助开发程序请执行这步,只是使用不需要.
+1. 安装python3.8 
+    - [Download](https://github.com/3038922/new_century_robotics/releases/download/v1.0/python-3.8.1-amd64.exe)
+
+
+
+
+## 自用命令记录
+### pyinstaller配置说明
+- 生成一大堆 `pyinstaller main.py`
+- 生成单exe `pyinstaller -F main.py`
+- 生成带图标文件的EXE `pyinstaller -F -i ./pic/机器人标.ico main.py`
